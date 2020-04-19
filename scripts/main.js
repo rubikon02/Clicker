47be872c31c96023c9969f0400f1c47019420846
rubikon02 <31660898+rubikon02@users.noreply.github.com>
2020-04-20 00:07:50 +0200

    click multiplier in localstorage

--- scripts/main.js
+++ scripts/main.js
@@ -11,7 +11,7 @@ let shop = new Array(myShop.length);
 //wczytanie punktów zapisanych lokalnie
 if(localStorage['points'] != null) {
     points = parseInt(localStorage.getItem('points'));
-    myPoints.textContent = 'Points: ' + points;
+    myPoints.textContent = points;
 } else {
     points = 0;
 }
@@ -30,29 +30,48 @@ if(localStorage['shop'] != null) {
     localStorage.setItem('shop', JSON.stringify(shop))
 }
 
+// wczytanie mnożnika kliknięcia zapisanego lokalnie
+if(localStorage['selectedClick'] != null) {
+    setClickValue(localStorage.getItem('selectedClick'));
+} else {
+    setClickValue(0)
+}
+
 //główny przycisk
 myButton.onclick = function() {
     points += parseInt(add);
-    myPoints.textContent = 'Points: ' + points;
+    myPoints.textContent = points;
     localStorage.setItem('points', parseInt(points));
 }
 
 //reset
 myReset.onclick = function() {
     points = 0;
-    myPoints.textContent = 'Points: ' + points;
+    myPoints.textContent = points;
     localStorage.setItem('points', 0);
+    localStorage.removeItem('shop')
     add = 1;
     for (let i = 1; i < myShop.length; i++) {
         myAdds[i].classList.add('disabled');
         myShop[i].classList.remove('disabled');
+        setClickValue(0);
     }
 }
 
-//wybór lepszego kliku
+// wybór mnożnika kliknięcia w localstorage
+function setClickValue(i) {
+    myAdds.forEach(function(entry) {
+        entry.classList.remove('deep-orange', 'lighten-1');
+    });
+    myAdds[i].classList.add('deep-orange', 'lighten-1');
+    add = myAdds[i].getAttribute("value");
+    localStorage.setItem('selectedClick', i);
+}
+
+//wybór mnożnika kliknięcia
 for (let i = 0; i < myAdds.length; i++) {
     myAdds[i].addEventListener("click", function() {
-        add = myAdds[i].getAttribute("value");
+        setClickValue(i);
     });
 }
 
@@ -61,7 +80,7 @@ for (let i = 0; i < myShop.length; i++) {
     myShop[i].addEventListener("click", function() {
         if (points >= myShop[i].getAttribute("value")) {
             points -= myShop[i].getAttribute("value");
-            myPoints.textContent = 'Points: ' + points;
+            myPoints.textContent = points;
             myAdds[i].classList.remove('disabled');
             myShop[i].classList.add('disabled');
 
